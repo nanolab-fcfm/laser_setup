83e2fbd2f91a9a269e036550846f6d0dfc7a0864
Benbriel <benjamin.briceno.bb@gmail.com>
2023-06-11 19:34:26 -0400

    Fix telegram test

--- tests/telegram_test.py
+++ tests/telegram_test.py
@@ -1,8 +1,7 @@
 from lib.display import send_telegram_alert
-from Scripts.IVg import IVg
 
 def test_telegram_alert():
-    send_telegram_alert('Benja', procedure=IVg)
+    send_telegram_alert('Hello World!')
 
 if __name__ == '__main__':
     test_telegram_alert()
