c222da21ba1699dc8499366c63f7f47e25583163
Benbriel <benjamin.briceno.bb@gmail.com>
2023-06-12 03:57:30 -0400

    Config/config.ini overrides default_config.ini

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,23 @@
+"""
+This package contains all the modules that are used in the
+experiment scripts. It is used to setup Pymeasure's config
+and logging, and to import the modules that are used in the
+experiment scripts.
+
+To overwrite the default config file, create a file called
+'./config/config.ini'.
+"""
 import os
 import logging
 
 from pymeasure.experiment.config import get_config, set_mpl_rcparams
 from pymeasure.log import setup_logging
+
+# Load the config file
+config_path = './config/config.ini'
+os.makedirs(os.path.dirname(config_path), exist_ok=True)
+if os.path.exists(config_path):
+    os.environ['CONFIG'] = config_path
 
 config = get_config()
 
@@ -14,4 +29,9 @@ if 'filename' in config['Logging']:
     os.makedirs(os.path.dirname(config['Logging']['filename']), exist_ok=True)
 
 setup_logging(log, **config['Logging'])
+
+config_using = 'default_config.ini' if 'CONFIG' not in os.environ.keys() else os.environ['CONFIG']
+log.info(f"Using config file: {config_using}")
+
+# Setup matplotlib.rcParams from config
 set_mpl_rcparams(config)
