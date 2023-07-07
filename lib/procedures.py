d1e33c216190e7723ebb83faa0129a5bdc464a49
nanolabfcfm <nanolab.fcfm@gmail.com>
2023-07-07 15:03:03 -0400

    Update procedures

--- lib/procedures.py
+++ lib/procedures.py
@@ -168,7 +168,7 @@ class ItBaseProcedure(Procedure):
     N_avg = IntegerParameter('N_avg', default=2)
     Irange = FloatParameter('Irange', units='A', default=0.001)
 
-    INPUTS = ['chip', 'sample', 'info', 'laser_wl', 'laser_T', 'laser_v', 'vds', 'vg', 'sampling_t', 'N_avg']
+    INPUTS = ['chip', 'chip_number', 'sample', 'info', 'laser_wl', 'laser_T', 'laser_v', 'vds', 'vg', 'sampling_t', 'N_avg']
     DATA_COLUMNS = ['t (s)', 'I (A)', 'VL (V)']
 
     def startup(self):
