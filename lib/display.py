49789a3ed6d996e29ae1869fde1aba6f43f9c9ce
Benbriel <benjamin.briceno.bb@gmail.com>
2023-07-24 22:32:24 -0400

    Add default dark mode to MainWindow

--- lib/display.py
+++ lib/display.py
@@ -8,6 +8,7 @@ from typing import Type
 from pymeasure.display.Qt import QtWidgets
 from pymeasure.display.windows import ManagedWindow
 from pymeasure.experiment import unique_filename, Results, Procedure
+from PyQt6.QtGui import QColor, QPalette
 from PyQt6.QtCore import QLocale
 
 from lib import config, log
@@ -60,12 +61,48 @@ def display_experiment(cls: Type[Procedure], title: str = ''):
     It also allows for existing data to be loaded and displayed.
     """
     app = QtWidgets.QApplication(sys.argv)
+    app.setStyle('Fusion')
+    app.setPalette(get_dark_palette())
     QLocale.setDefault(QLocale(QLocale.Language.English, QLocale.Country.UnitedStates))
     window = MainWindow(cls, title)
     window.show()
     app.exec()
     remove_empty_data()
     sys.exit()
+
+
+def get_dark_palette():
+    palette = QPalette()
+
+    # Set the background color
+    palette.setColor(QPalette.ColorRole.Window, QColor(50, 50, 50))
+
+    # Set the title text color
+    palette.setColor(QPalette.ColorRole.WindowText, QColor(200, 200, 200))
+
+    # Set the input text color
+    palette.setColor(QPalette.ColorRole.Text, QColor(200, 200, 200))
+
+    # Set the button color
+    palette.setColor(QPalette.ColorRole.Button, QColor(30, 30, 30))
+
+    # Set the button text color
+    palette.setColor(QPalette.ColorRole.ButtonText, QColor(200, 200, 200))
+
+    # Set the base color
+    palette.setColor(QPalette.ColorRole.Base, QColor(35, 35, 35))
+
+    # Set the alternate background color
+    palette.setColor(QPalette.ColorRole.AlternateBase, QColor(45, 45, 45))
+
+    # Set the highlight color
+    palette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130, 218))
+
+    # Set the highlighted text color
+    palette.setColor(QPalette.ColorRole.HighlightedText, QColor(240, 240, 240))
+
+    return palette
+
 
 def send_telegram_alert(message: str):
     """Sends a message to all valid Telegram chats on config['Telegram'].
