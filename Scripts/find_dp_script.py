dd21942d9f3d34405bb132c1c86ae7bb3bc7f038
nanolabfcfm <nanolab.fcfm@gmail.com>
2023-10-10 15:07:35 -0300

    added interface to select file in find_dp_script

--- Scripts/find_dp_script.py
+++ Scripts/find_dp_script.py
@@ -1,5 +1,22 @@
+"""
+This Script is used to find the dirac point of a selected IVg curve.
+"""
 from Scripts.utils import *
-path_to_file = r"C:\Users\nanol\nanolab\laser_setup\data\2023-09-07\IVg2023-09-07_9.csv"
-data = read_pymeasure(path_to_file)
-dp = find_dp(data)
-print(dp)
+
+from datetime import datetime
+from tkinter import Tk     
+from tkinter.filedialog import askopenfilename
+
+
+if __name__ == "__main__":
+    current_datetime = datetime.now()
+    todays_date = f"{current_datetime.year}-{current_datetime.month}-{current_datetime.day}"
+    todays_folder_path = f"C:/Users/nanol/nanolab/laser_setup/data/{todays_date}/"
+
+    Tk().withdraw()
+    path_to_file = askopenfilename(initialdir=todays_folder_path,
+                                title="Select IVg to find DP")
+
+    data = read_pymeasure(path_to_file)
+    dp = find_dp(data)
+    print(f"File: '{path_to_file.split('/')[-1]}' \t DP = {dp} [V]")
