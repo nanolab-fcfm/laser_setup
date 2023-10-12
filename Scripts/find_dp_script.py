532b4f69699fc09e922ddbe8aa0585932d58a465
nanolabfcfm <nanolab.fcfm@gmail.com>
2023-10-12 18:31:08 -0300

    fixed path in find_dp_script

--- Scripts/find_dp_script.py
+++ Scripts/find_dp_script.py
@@ -3,6 +3,7 @@ This Script is used to find the dirac point of a selected IVg curve.
 """
 from Scripts.utils import *
 
+import os
 from datetime import datetime
 from tkinter import Tk     
 from tkinter.filedialog import askopenfilename
@@ -11,7 +12,7 @@ from tkinter.filedialog import askopenfilename
 if __name__ == "__main__":
     current_datetime = datetime.now()
     todays_date = f"{current_datetime.year}-{current_datetime.month}-{current_datetime.day}"
-    todays_folder_path = f"C:/Users/nanol/nanolab/laser_setup/data/{todays_date}/"
+    todays_folder_path = f"{os.getcwd()}/data/{todays_date}/"
 
     Tk().withdraw()
     path_to_file = askopenfilename(initialdir=todays_folder_path,
