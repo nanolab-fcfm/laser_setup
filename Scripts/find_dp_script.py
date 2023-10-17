ed3653b2db16eeefa1229bec29d3856000a92caf
nanolabfcfm <nanolab.fcfm@gmail.com>
2023-10-17 18:25:20 -0300

    Added multiple file selection in find_dp_script

--- Scripts/find_dp_script.py
+++ Scripts/find_dp_script.py
@@ -6,18 +6,19 @@ from Scripts.utils import *
 import os
 from datetime import datetime
 from tkinter import Tk     
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilenames
 
 
 if __name__ == "__main__":
     current_datetime = datetime.now()
     todays_date = f"{current_datetime.year}-{current_datetime.month}-{current_datetime.day}"
     todays_folder_path = f"{os.getcwd()}/data/{todays_date}/"
-
+    
     Tk().withdraw()
-    path_to_file = askopenfilename(initialdir=todays_folder_path,
+    path_to_files = askopenfilenames(initialdir=todays_folder_path,
                                 title="Select IVg to find DP")
 
-    data = read_pymeasure(path_to_file)
-    dp = find_dp(data)
-    print(f"File: '{path_to_file.split('/')[-1]}' \t DP = {dp} [V]")
+    for path in path_to_files:
+        data = read_pymeasure(path)
+        dp = find_dp(data)
+        print(f"File: '{path.split('/')[-1]}' \t DP = {dp} [V]")
