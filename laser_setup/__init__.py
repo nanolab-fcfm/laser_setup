85c8428a0fdcdec41cd14ab2c2d949f653b81266
benbriel <benjamin.briceno.bb@gmail.com>
2024-05-05 23:59:52 +0200

    Replace requirements.txt with pyproject.toml

--- laser_setup/__init__.py
+++ laser_setup/__init__.py
@@ -13,9 +13,11 @@ import logging
 from pymeasure.experiment.config import get_config, set_mpl_rcparams
 from pymeasure.log import setup_logging
 
+__version__ = '0.3.0-alpha'
+
 # Load the config files
 _default_config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'default_config.ini'))
-config_path = './config/config.ini' # os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'config', 'config.ini') 
+config_path = './config/config.ini' # os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), 'config', 'config.ini')
 os.makedirs(os.path.dirname(config_path), exist_ok=True) # [os.path.join('config', d) for d in os.listdir('config')]
 with open(config_path, 'a') as f: pass
 
@@ -26,10 +28,10 @@ config = get_config([_default_config_path, config_path])
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
-if 'filename' in config['Logging']:
+if config.has_option('Logging', 'filename'):
     os.makedirs(os.path.dirname(config['Logging']['filename']), exist_ok=True)
 
-setup_logging(log, **config['Logging'])
+setup_logging(log, **config['Logging'] if config.has_section('Logging') else {})
 
 _config_file_used = config_path
 log.info(f"Using config file: {_config_file_used}")
