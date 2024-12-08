af22f6ee125ac3ee158b7b32c574d48f4049592c
benbriel <benjamin.briceno.bb@gmail.com>
2024-12-08 07:21:50 +0100

    Added Worker for threaded code execution

--- laser_setup/display/Qt.py
+++ laser_setup/display/Qt.py
@@ -16,3 +16,30 @@ if not TYPE_CHECKING:
 else:
     # For type checking, PyQt6 is used
     from PyQt6 import QtSql
+
+
+class Worker(QtCore.QObject):
+    """Worker class to run a function in a separate thread. The result
+    is emitted as a signal. If the thread is specified, the worker is
+    moved to that thread, and the thread is stopped when the worker is
+    done.
+    """
+    finished = QtCore.pyqtSignal(object)
+    def __init__(
+        self,
+        func: callable,
+        thread: QtCore.QThread = None,
+        **kwargs
+    ):
+        super().__init__()
+        self.func = func
+        self.kwargs = kwargs
+        if thread is not None:
+            self.moveToThread(thread)
+            thread.started.connect(self.run)
+            thread.finished.connect(self.deleteLater)
+            thread.finished.connect(thread.deleteLater)
+
+    def run(self):
+        result = self.func(**self.kwargs)
+        self.finished.emit(result)
