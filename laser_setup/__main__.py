771d5a36e42bbe673108b290cc393aa1a9a9800e
benbriel <benjamin.briceno.bb@gmail.com>
2024-12-14 05:01:53 +0100

    Add --version cli argument

--- laser_setup/__main__.py
+++ laser_setup/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 
+from . import __version__
 from .display import MainWindow, ExperimentWindow, display_window
 from .cli import Scripts, script_list
 from .procedures import Experiments, experiment_list
@@ -8,7 +9,9 @@ from .procedures import Experiments, experiment_list
 def main():
     parser = argparse.ArgumentParser(description='Laser Setup')
     parser.add_argument('procedure', nargs='?', help='Procedure to run', choices=experiment_list + script_list)
+    parser.add_argument('-v', '--version', action='version', version=f'%(prog)s {__version__}')
     parser.add_argument('-d', '--debug', action='store_true', default=False, help='Enable debug mode')
+
     args = parser.parse_args()
 
     if args.procedure is None:
