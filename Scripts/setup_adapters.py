7197d2ed809316b9df6d76f0fd1922f96f817433
benbriel <benjamin.briceno.bb@gmail.com>
2025-01-18 04:23:52 +0100

    Remove Scripts directory and contents

--- Scripts/setup_adapters.py
+++
@@ -1,9 +0,0 @@
-"""
-This interactive script is used to configure the USB adapters that are used in
-the experiment scripts. It is based on the Pymeasure library.
-"""
-from laser_setup.cli.setup_adapters import main
-
-
-if __name__ == '__main__':
-    main()
