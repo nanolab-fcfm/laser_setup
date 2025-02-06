cccd3819964581bd38c505540e341e460178fc0d
benbriel <benjamin.briceno.bb@gmail.com>
2025-02-06 05:22:34 +0100

    Refactor BaseProcedure; remove metaclass, add features

--- laser_setup/procedures/BaseProcedure.py
+++ laser_setup/procedures/BaseProcedure.py
@@ -1,8 +1,10 @@
 import logging
 import time
+from functools import wraps
 
 from omegaconf import DictConfig
-from pymeasure.experiment import Procedure
+from pymeasure.experiment import (BooleanParameter, Metadata, Parameter,
+                                  Procedure)
 
 from ..config import config, load_yaml
 from ..instruments import InstrumentManager, PendingInstrument
@@ -13,52 +15,51 @@ log = logging.getLogger(__name__)
 procedure_config = load_yaml(config.Dir.procedure_config_file, _instantiate=True)
 
 
-class BaseProcedureMeta(type):
-    """Metaclass for BaseProcedure. It is used to set attributes
-    and the default values of `Parameters` in the class definition.
-    """
-    def __new__(cls: type, name: str, bases: tuple, dct: dict):
-        procedure_dict: dict = procedure_config.get(name, {})
-
-        for key, value in procedure_dict.pop('update_parameters', {}).items():
-            if key not in dct:
-                continue
-
-            if not isinstance(value, (dict, DictConfig)):
-                value = {'value': value}
-
-            for k, v in value.items():
-                try:
-                    setattr(dct[key], k, v)
-                except AttributeError:
-                    log.error(f"Error updating parameter {key} in {name}")
-
-        dct.update(procedure_dict)
-        return super().__new__(cls, name, bases, dct)
-
-
-class BaseProcedure(Procedure, metaclass=BaseProcedureMeta):
+class BaseProcedure(Procedure):
     """Base procedure for all measurements. It defines basic
-    parameters that have to be present in all procedures.
+    parameters that are present in all procedures. It also provides
+    methods for connecting instruments and shutting down an experiment.
+    You can override any of the attributes or methods in a subclass.
+
+    :attr name: Name of the procedure,
+    :attr instruments: InstrumentManager instance
+    :attr procedure_version: Version of the procedure
+    :attr show_more: Show more parameters
+    :attr info: Information about the procedure
+    :attr exec_startup: Execute startup
+    :attr exec_shutdown: Execute shutdown
+    :attr start_time: Start time of the procedure
+    :attr time: Time module
+    :attr INPUTS: List of input parameters to be displayed
+    :attr EXCLUDE: List of parameters to exclude from the save file
+    :attr DATA_COLUMNS: List of data columns
+    :attr SEQUENCER_INPUTS: List of inputs for the sequencer
+
+    :method connect_instruments: Connects all PendingInstruments to the
+        InstrumentManager
+    :method shutdown: Shuts down all instruments
+    :method __init_subclass__: Updates parameters and class attributes
+        for subclasses, based on the procedures config file
     """
-    name: str = None
+    name: str = ""
     # Instrument Manager
     instruments = InstrumentManager()
 
-    procedure_version = Parameters.Base.procedure_version
-    show_more = Parameters.Base.show_more
-    info = Parameters.Base.info
+    procedure_version = Parameter("Procedure version", default="1.0.0")
+    show_more = BooleanParameter("Show more", default=False)
+    info = Parameter("Information", default="None")
 
-    # Chained Execution
-    chained_exec = Parameters.Base.chained_exec
-    startup_executed = False
+    # Startup and shutdown execution
+    skip_startup = BooleanParameter("Skip startup", default=False, group_by='show_more')
+    skip_shutdown = BooleanParameter("Skip shutdown", default=False, group_by='show_more')
 
     # Metadata
-    start_time = Parameters.Base.start_time
+    start_time = Metadata("Start time", fget="time.time")
     # Access to time module as attribute for Metadata.fget
     time = time
 
-    INPUTS = ['show_more', 'chained_exec', 'info']
+    INPUTS = ['show_more', 'skip_startup', 'skip_shutdown', 'info']
+    EXCLUDE = ['show_more', 'skip_startup', 'skip_shutdown']
 
     def connect_instruments(self):
         """Takes all PendingInstruments and connects them to the
@@ -73,12 +74,45 @@ class BaseProcedure(Procedure, metaclass=BaseProcedureMeta):
                 setattr(self, key, self.instruments.connect(**instr_dict))
 
     def shutdown(self):
-        if not self.should_stop() and self.status >= self.RUNNING and self.chained_exec:
-            log.info("Skipping shutdown")
-            return
-
         self.instruments.shutdown_all()
-        self.__class__.startup_executed = False
+
+    def __init_subclass__(cls: type['BaseProcedure'], **kwargs):
+        super().__init_subclass__(**kwargs)
+        procedure_dict: dict = procedure_config.get(cls.__name__, {})
+        for key, value in procedure_dict.pop('update_parameters', {}).items():
+            if not (param := getattr(cls, key, None)) or \
+               not isinstance(param, (Parameter, Metadata)):
+                continue
+
+            if not isinstance(value, (dict, DictConfig)):
+                value = {'value': value}
+
+            for k, v in value.items():
+                try:
+                    setattr(param, k, v)
+                except AttributeError:
+                    log.error(f"Error updating parameter {key} in {cls.__name__}")
+
+            setattr(cls, key, param)
+
+        for key, value in procedure_dict.items():
+            setattr(cls, key, value)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Wrap methods to skip execution
+        self.startup = self._wrap_skip(self.startup, 'skip_startup')
+        self.shutdown = self._wrap_skip(self.shutdown, 'skip_shutdown')
+
+    def _wrap_skip(self, method, flag_name: str):
+        @wraps(method)
+        def wrapper(*args, **kwargs):
+            if getattr(self, flag_name, False):
+                log.info(f"Skipping {method.__name__} for {self.__class__.__name__}")
+                self.connect_instruments()
+                return
+            return method(*args, **kwargs)
+        return wrapper
 
 
 class ChipProcedure(BaseProcedure):
@@ -99,9 +133,4 @@ class ChipProcedure(BaseProcedure):
                 f"{self.chip_group} {self.chip_number}, Sample {self.sample}!"
             )
 
-            if self.chained_exec:
-                log.info("Skipping shutdown")
-                return
-
-        self.instruments.shutdown_all()
-        self.__class__.startup_executed = False
+        super().shutdown()
