d1e6ee9de5898b458821cdbf011cba8e1aca188e
benjamin.briceno <benjaminbriceno@ug.uchile.cl>
2025-02-25 13:44:30 +0000

    Run flow 01953d46-0407-7a61-ae22-56bf75dd5314

--- tests/ramp_test.py
+++ tests/ramp_test.py
@@ -1,13 +1,19 @@
+import balthazar as blt
 import matplotlib.pyplot as plt
 import numpy as np
 
 from laser_setup.utils import voltage_sweep_ramp
 
+v_start = blt.params["v_start"]
+v_stop = blt.params["v_stop"]
+v_points = blt.params["v_points"]
+
+
 def test_ramp():
-    ramp = voltage_sweep_ramp(-35, 35, 0.5)
+    ramp = voltage_sweep_ramp(v_start, v_stop, v_points)
     return ramp
 
 if __name__ == '__main__':
     ramp = test_ramp()
-    plt.scatter(np.linspace(-35, 35, len(ramp)), ramp)
+    plt.scatter(np.linspace(v_start, v_stop, len(ramp)), ramp)
     plt.show()
